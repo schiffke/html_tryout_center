7f0eed93cca0169772976bc2fb862797f4adc097
Nick Schiffke <Nick.Schiffke@strato.de>
2021-03-17 15:39:12 +0100

    feat[progress-bar] added red vs green progress bar battle minigame

--- index.js
+++ index.js
@@ -1,35 +1,17 @@
-var lowerCaseStr = "should pass the basic tests"
-
-function isLowerCaseLetter(c) {
-  if (c.charCodeAt(0) <= 122 && c.charCodeAt(0) >= 97) {
-    return true
-  }
-  return false
-}
-
-function toUpperCase(c) {
-  if (isLowerCaseLetter(c)) {
-    return (String.fromCharCode(c.charCodeAt(0) - 32))
-  }
-  return c
-}
-
-function getCharacterLength(str) {
-  return [...str].length;
-}
-
-function jadenCase(str) {
-  str = " " + str
-  var x = getCharacterLength(str)
+function extract_value(s) {
+  var nums = "1234567890";
   var out = ""
-  for (i = 0; i < x; i++) {
-    if (!isLowerCaseLetter(str[i])) {
-      i++
-      out += str[i]
-    } else {
-      out += str[i]
+  for (var i = 0; i < s.length; i++) {
+    if (nums.includes(s[i])) {
+      out += s[i]
     }
   }
+  return out
 }
-// Leerzeichen an den Anfang, immer nur einmal uppercase nachdem isLowerCaseLetter == false 
-console.log(jadenCase("hallo mein name ist nick"))
+
+
+function button_press(x) {
+  var bar = document.getElementById("progressbar_top");
+  var progress = parseInt(extract_value(bar.getAttribute("style")));
+  bar.style.width = (progress + x) + "%"
+}
