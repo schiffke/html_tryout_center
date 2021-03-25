6ec0544a9d67be94380c0275f873ac8ecccb0b4d
Nick Schiffke <Nick.Schiffke@strato.de>
2021-03-25 12:17:11 +0100

    testo sterone

--- code.js
+++ code.js
@@ -1,4 +1,30 @@
-import { execSync } from "child_process";
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        let date = this.responseText.replace(/[TZ\]\[\"\""]/g, " ")
+        document.getElementById("output").innerHTML = date;
+       }
+    };
+    xhttp.open("GET", "date.json", true);
+    xhttp.send();
+  }
+
+loadDoc()
+
+
+// 'use strict';
+/*
+import * as rawdata from "date.json";
+
+// let rawdata = fs.readFileSync('date.json');
+let date = JSON.parse(rawdata);
+console.log(date[0]);
+
+
+
+
+ import { execSync } from "child_process";
 
 const gitCommand = "git show -s --format=%ci";
 
@@ -6,5 +32,7 @@ export default function getGitCommitDate() {
     return execSync(gitCommand).toString().trim();
 }
 
-console.log("hm")
-console.log("hi" + getGitCommitDate())
+var date = getGitCommitDate();
+
+document.getElementById("output").textContent = date[0];
+*/
